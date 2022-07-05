08a5ebe1be418a3952516358a3dda53e26455027
Jonh Alex <jonhpaz08@gmail.com>
2022-07-05 03:23:58 -0300

    filtro de letras no email

--- form-contato/processa.php
+++ form-contato/processa.php
@@ -1,7 +1,7 @@
-<?php
+    <?php
 
-$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 
-echo "E-mail: $email <br>";
-echo "Senha: $senha <br>";
+    echo "E-mail: $email <br>";
+    echo "Senha: $senha <br>";
